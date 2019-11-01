21782df01e29022435d87576d4a6887bdcf1c6b6
Laurent Girod <laurent.girod@epfl.ch>
2019-11-01 10:47:02 +0100

    Improve benchmarks.

--- test/benchmark.py
+++ test/benchmark.py
@@ -14,6 +14,12 @@ from sscred.acl import (
     ACLUser
 )
 
+from sscred.blind_signature import (
+    AbeParam,
+    AbeSigner,
+    AbeUser
+)
+
 
 def perf_measure_call(fn, *args, **kwargs):
     "Measure the execution time of a function."
@@ -38,6 +44,81 @@ def perf_measure_call(fn, *args, **kwargs):
         gc.enable()
 
     return ret, t_end - t_begin
+
+
+class AbeBenchmark:
+    "Contains the benchmark data for the ACL module."
+
+    def __init__(self, n_repetitions=1000):
+        self.n_repetitions = n_repetitions
+
+        self.t_abe_param_init = list()
+        self.t_generate_new_key_pair = list()
+        self.t_abe_signer_init = list()
+        self.t_abe_user_init = list()
+        self.t_abe_signer_commit = list()
+        self.t_user_compute_blind_challenge = list()
+        self.t_signer_respond = list()
+        self.t_user_compute_signature = list()
+        self.t_signer_pub_verify_signature = list()
+
+
+    def run(self):
+        "Run a complete encryption and store the execution time of the methods executed."
+
+        random.seed(42)
+
+        for _ in range(self.n_repetitions):
+            abe_param, t = perf_measure_call(AbeParam)
+            self.t_abe_param_init.append(t)
+
+            keys, t = perf_measure_call(abe_param.generate_new_key_pair)
+            self.t_generate_new_key_pair.append(t)
+
+            signer_priv, signer_pub = keys
+
+            signer, t = perf_measure_call(AbeSigner, signer_priv, signer_pub)
+            self.t_abe_signer_init.append(t)
+
+            user, t = perf_measure_call(AbeUser, signer_pub)
+            self.t_abe_user_init.append(t)
+
+            com, t = perf_measure_call(signer.commit)
+            self.t_abe_signer_commit.append(t)
+
+            message = ''.join([random.choice(string.printable) for _ in range(100)])
+
+            challenge, t = perf_measure_call(user.compute_blind_challenge, com, message)
+            self.t_user_compute_blind_challenge.append(t)
+
+            resp, t = perf_measure_call(signer.respond, challenge)
+            self.t_signer_respond.append(t)
+
+            sig, t = perf_measure_call(user.compute_signature, resp)
+            self.t_user_compute_signature.append(t)
+
+            _, t = perf_measure_call(signer_pub.verify_signature, sig)
+            self.t_signer_pub_verify_signature.append(t)
+
+
+    def save(self, filename='abe-benchmark.json'):
+        "Save the collected data in json format in a file which name is given in argument."
+
+        file_struct = {}
+        file_struct['abe_param_init'] = self.t_abe_param_init
+        file_struct['generate_new_key_pair'] = self.t_generate_new_key_pair
+        file_struct['abe_signer_init'] = self.t_abe_signer_init
+        file_struct['abe_user_init'] = self.t_abe_user_init
+        file_struct['abe_signer_commit'] = self.t_abe_signer_commit
+        file_struct['user_compute_blind_challenge'] = self.t_user_compute_blind_challenge
+        file_struct['signer_respond'] = self.t_signer_respond
+        file_struct['user_compute_signature'] = self.t_user_compute_signature
+        file_struct['signer_pub_verify_signature'] = self.t_signer_pub_verify_signature
+
+        file_content = json.dumps(file_struct)
+
+        with open(filename, 'w') as fd:
+            fd.write(file_content)
 
 
 class ACLBenchmark:
@@ -111,7 +192,7 @@ class ACLBenchmark:
             self.t_cred_verify_credential.append(t)
 
 
-    def save(self, filename):
+    def save(self, filename='acl-benchmark.json'):
         "Save the collected data in json format in a file which name is given in argument."
 
         file_struct = {}
@@ -134,9 +215,13 @@ class ACLBenchmark:
 
 
 if __name__ == '__main__':
-    benchmark = ACLBenchmark()
-    gc.disable()
-    benchmark.run()
-    gc.enable()
+    Benchmark = AbeBenchmark
 
-    benchmark.save('acl-benchmark.json')
+    if Benchmark:
+        benchmark = Benchmark()
+        gc.disable()
+        benchmark.run()
+        gc.enable()
+
+        benchmark.save()
+
